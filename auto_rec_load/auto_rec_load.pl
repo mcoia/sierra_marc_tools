e2f50dc33ceee10634bbc7e268b0bf8581cb46de
blake <blake@mobiusconsortium.org>
2021-08-19 15:29:46 -0500

    Auto record loader stub and some cleanup

---
+++ auto_rec_load/auto_rec_load.pl
@@ -0,0 +1,564 @@
+#!/usr/bin/perl
+
+
+use lib qw(../); 
+use Loghandler;
+use Data::Dumper;
+use File::Path qw(make_path remove_tree);
+use File::Copy;
+use DBhandler;
+use Encode;
+use Text::CSV;
+use DateTime;
+use DateTime::Format::Duration;
+use DateTime::Span;
+use JSON;
+use Selenium::Remote::Driver;
+use Selenium::Firefox;
+use Selenium::Remote::WebElement;
+use pQuery;
+use Getopt::Long;
+use Cwd;
+
+use website;
+use innreachServer;
+
+our $stagingTablePrefix = "auto_rec_load";
+our $pidfile = "/tmp/auto_rec_load.pl.pid";
+
+
+our $driver;
+our $dbHandler;
+our $databaseName = '';
+our $log;
+our $debug = 0;
+our $recreateDB = 0;
+our $dbSeed;
+our $specificSource;
+our $configFile;
+our $jobid = -1;
+our $vendor = "";
+
+GetOptions (
+"log=s" => \$log,
+"config=s" => \$configFile,
+"debug" => \$debug,
+"recreateDB" => \$recreateDB,
+"dbSeed=s" => \$dbSeed,
+"specificSource=s" => \$specificSource,
+)
+or die("Error in command line arguments\nYou can specify
+--log path_to_log_output.log                  [Path to the log output file - required]
+--config                                      [Path to the config file]
+--debug                                       [Cause more log output]
+--recreateDB                                  [Deletes the tables and recreates them]
+--dbSeed                                      [DB Seed file - populating the base data]
+
+\n");
+
+if(!$configFile || !(-e $configFile) )
+{
+    print "Please specify a valid path to a config file\n";
+    exit;
+}
+
+our $mobUtil = new Mobiusutil();
+our $conf = $mobUtil->readConfFile($configFile);
+
+
+if($conf)
+{
+    %conf = %{$conf};
+    if ($conf{"logfile"})
+    {
+        figurePIDFileStuff();
+        checkConfig();
+        $log = new Loghandler($conf->{"logfile"});
+        $log->truncFile("****************** Starting ******************");
+
+        setupDB();
+
+        createDatabase();
+
+        initializeBrowser();
+
+        my $writePid = new Loghandler($pidfile);
+        $writePid->truncFile("running");
+
+        my $cwd = getcwd();
+        $cwd .= "/screenshots";
+        mkdir $cwd unless -d $cwd;
+
+        my %all = %{getSources()};
+        while ( (my $key, my $value) = each(%all) )
+        {
+            my %details = %{$value};
+            if(!(-d $details{"outputfolder"}))
+            {
+                print "Scheduler Output folder: '".$details{"outputfolder"}."' doesn't exist\n";
+                exit;
+            }
+            print "Working on: '$key'\n";
+
+            my $source;
+            if($clusters{$_} =~ m/sierra/)  ## Right now, there are two typs: sierra and innreach
+            {
+                $cluster = new sierraCluster($_,$dbHandler,$stagingTablePrefix,$driver,$cwd,$monthsBack,$blindDate,$log,$debug,$specificMonth);
+            }
+            else
+            {
+                $cluster = new innreachServer($_,$dbHandler,$stagingTablePrefix,$driver,$cwd,$monthsBack,$blindDate,$log,$debug,$specificMonth);
+            }
+            $cluster->scrape();
+
+        }
+
+        undef $writePid;
+        closeBrowser();
+        $log->addLogLine("****************** Ending ******************");
+    }
+    else
+    {
+        
+    }
+}
+else
+{
+    print "Something went wrong with the config\n";
+    exit;
+}
+
+sub getSources
+{
+    my @ret = ();
+    my %sources = ();
+    my @order = ();
+    my $query = "
+    SELECT
+    client.name,source.name,source.type,source.scheduler_folder,source.json_connection_detail
+    FROM
+    $stagingTablePrefix"."_client client
+    join $stagingTablePrefix"."_source source on (source.client=client.id)
+    where
+    client.id=client.id
+    ___specific___
+    order by 2 desc,1
+    ";
+
+    # defang
+    undef $specificSource if($specificSource =~ m/[&'%\\\/]/);
+
+    $query =~ s/___specific___/AND LOWER(source.name) = '$specificSource'/g if $specificSource;
+    $query =~ s/___specific___//g if !$specificSource;
+    $log->addLogLine($query);
+    my @results = @{$dbHandler->query($query)};
+    foreach(@results)
+    {
+        my @row = @{$_};
+        my %hash = ();
+        $hash{"sourcename"} = @row[1];
+        $hash{"type"} = @row[2];
+        $hash{"outputfolder"} = @row[3];
+        $hash{"json"} = @row[4];
+        $sources{@row[0]} = \%hash;
+    }
+    return \%sources;
+}
+
+sub escapeData
+{
+    my $d = shift;
+    $d =~ s/'/\\'/g;
+    return $d;
+}
+
+sub unEscapeData
+{
+    my $d = shift;
+    $d =~ s/\\'/'/g;
+    return $d;
+}
+
+sub initializeBrowser
+{
+    $Selenium::Remote::Driver::FORCE_WD3=1;
+
+    # my $driver = Selenium::Firefox->new();
+    $driver = Selenium::Remote::Driver->new
+        (
+            binary => '/usr/bin/geckodriver',
+            browser_name  => 'firefox',
+        );
+    $driver->set_window_size(1200,3500); # Need it wide to capture more of the header on those large HTML tables
+}
+
+sub closeBrowser
+{
+    $driver->quit;
+
+    # $driver->shutdown_binary;
+}
+
+sub setupDB
+{
+    eval{$dbHandler = new DBhandler($conf{"db"},$conf{"dbhost"},$conf{"dbuser"},$conf{"dbpass"},$conf{"port"}||"3306","mysql");};
+    if ($@)
+    {
+        print "Could not establish a connection to the database\n";
+        alertErrorEmail("Could not establish a connection to the database");
+        exit 1;
+    }
+    $databaseName = $conf{"db"};
+}
+
+sub createDatabase
+{
+
+    if($recreateDB)
+    {
+        my $query = "DROP TABLE $stagingTablePrefix"."_import_status ";
+        $log->addLine($query);
+        $dbHandler->update($query);
+        my $query = "DROP TABLE $stagingTablePrefix"."_file_track ";
+        $log->addLine($query);
+        $dbHandler->update($query);
+        my $query = "DROP TABLE $stagingTablePrefix"."_source ";
+        $log->addLine($query);
+        $dbHandler->update($query);
+        my $query = "DROP TABLE $stagingTablePrefix"."_client ";
+        $log->addLine($query);
+        $dbHandler->update($query);
+        my $query = "DROP TABLE $stagingTablePrefix"."_job ";
+        $log->addLine($query);
+        $dbHandler->update($query);
+    }
+
+    my @exists = @{$dbHandler->query("SELECT table_name FROM information_schema.tables WHERE table_schema RLIKE '$databaseName' AND table_name RLIKE '$stagingTablePrefix'")};
+    if(!$exists[0])
+    {
+    
+        ##################
+        # TABLES
+        ##################
+        my $query = "CREATE TABLE $stagingTablePrefix"."_job (
+        id int not null auto_increment,
+        start_time datetime DEFAULT CURRENT_TIMESTAMP,
+        last_update_time datetime DEFAULT CURRENT_TIMESTAMP,
+        current_action varchar(1000),
+        status varchar(100) default 'initializing',
+        current_action_num int DEFAULT 0,
+        PRIMARY KEY (id)
+        )
+        ";
+        $log->addLine($query) if $debug;
+        $dbHandler->update($query);
+
+        $query = "CREATE TABLE $stagingTablePrefix"."_client (
+        id int not null auto_increment,
+        name varchar(100),
+        PRIMARY KEY (id),
+        UNIQUE INDEX (name)
+        )
+        ";
+        $log->addLine($query) if $debug;
+        $dbHandler->update($query);
+
+        $query = "CREATE TABLE $stagingTablePrefix"."_source (
+        id int not null auto_increment,
+        name varchar(100),
+        type varchar(100),
+        client int,
+        scheduler_folder varchar(500),
+        json_connection_detail varchar(5000),
+        PRIMARY KEY (id),
+        FOREIGN KEY (client) REFERENCES $stagingTablePrefix"."_client(id) ON DELETE CASCADE
+        )
+        ";
+        $log->addLine($query) if $debug;
+        $dbHandler->update($query);
+
+        $query = "CREATE TABLE $stagingTablePrefix"."_file_track (
+        id int not null auto_increment,
+        name varchar(1000),
+        client int,
+        source int,
+        size int,
+        grab_time datetime DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        INDEX (name),
+        FOREIGN KEY (client) REFERENCES $stagingTablePrefix"."_client(id) ON DELETE CASCADE,
+        FOREIGN KEY (source) REFERENCES $stagingTablePrefix"."_source(id) ON DELETE CASCADE
+        )
+        ";
+        $log->addLine($query) if $debug;
+        $dbHandler->update($query);
+        
+        $query = "CREATE TABLE $stagingTablePrefix"."_import_status (
+        id int not null auto_increment,
+        file int,
+        status varchar(100),
+        record_raw mediumtext,
+        record_tweaked mediumtext,
+        insert_time datetime DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        FOREIGN KEY (file) REFERENCES $stagingTablePrefix"."_file_track(id) ON DELETE CASCADE
+        )
+        ";
+        $log->addLine($query) if $debug;
+        $dbHandler->update($query);
+
+        seedDB($dbSeed) if $dbSeed;
+
+    }
+    else
+    {
+        print "Staging table already exists\n";
+    }
+}
+
+sub seedDB
+{
+    my $seedFile = shift;
+    my $readFile = new Loghandler($seedFile);
+    $log->addLine("Reading seeDB File $seedFile");
+    my @lines = @{$readFile->readFile()};
+
+    my $currTable = '';
+    my @cols = ();
+    my $insertQuery = "";
+    my @datavals = ();
+    foreach(@lines)
+    {
+        my $line = $_;
+        $line = trim($line);
+        if($line =~ m/^\[/)
+        {
+            if( ($#cols > -1) && ($#datavals > -1) )
+            {
+                # execute the insert
+                @flatVals = ();
+                my $insertLog = $insertQuery;
+                foreach(@datavals)
+                {
+                    my @row = @{$_};
+                    $insertQuery .= "(";
+                    $insertLog .= "(";
+                    $insertQuery .= ' ? ,' foreach(@row);
+                    $insertLog .= " '$_' ," foreach(@row);
+                    $insertQuery = substr($insertQuery,0,-1);
+                    $insertLog = substr($insertLog,0,-1);
+                    $insertQuery .= "),\n";
+                    $insertLog .= "),\n";
+                    push @flatVals, @row;
+                }
+                $insertQuery = substr($insertQuery,0,-2);
+                $insertLog = substr($insertLog,0,-2);
+                $log->addLine($insertLog);
+                $dbHandler->updateWithParameters($insertQuery,\@flatVals);
+                undef @flatVals;
+                @datavals = ();
+            }
+            $log->addLine("seedDB: Detected client delcaration") if $debug;
+            $currTable = $line;
+            $currTable =~ s/^\[([^\]]*)\]/$1/g;
+            $log->addLine("Heading $currTable") if $debug;
+            @cols = @{figureColumnsFromTable($currTable)};
+            my @temp = ();
+            $insertQuery = "INSERT INTO $stagingTablePrefix"."_$currTable (";
+            foreach(@cols)
+            {
+                $insertQuery .= "$_," if($_ ne 'id');
+                push @temp, $_ if($_ ne 'id');
+            }
+            @cols = @temp;
+            undef @temp;
+            $insertQuery = substr($insertQuery,0,-1);
+            $insertQuery .= ")\nvalues\n";
+            $log->addLine(Dumper(\@cols)) if $debug;
+            $log->addLine(Dumper($#cols)) if $debug;
+        }
+        elsif($currTable)
+        {
+            $log->addLine($line);
+            
+            my @vals = split(/\t/,$line);
+            $log->addLine("Split and got\n".Dumper(\@vals)) if $debug;
+            $log->addLine("Expecting $#cols and got $#vals") if $debug;
+            if($#vals == $#cols) ## Expected number of columns
+            {
+                my @v = ();
+                my $colPos = 0;
+                foreach (@vals)
+                {
+                    my $val = getForignKey($currTable, $colPos, $_);
+                    push @v, $val;
+                    $colPos++;
+                }
+                push @datavals, [@v];
+            }
+        }
+    }
+    if( ($#cols > -1) && ($#datavals > -1) )
+    {
+        # execute the insert
+        @flatVals = ();
+        my $insertLog = $insertQuery;
+        foreach(@datavals)
+        {
+            my @row = @{$_};
+            $insertQuery .= "(";
+            $insertLog .= "(";
+            $insertQuery .= ' ? ,' foreach(@row);
+            $insertLog .= " '$_' ," foreach(@row);
+            $insertQuery = substr($insertQuery,0,-1);
+            $insertLog = substr($insertLog,0,-1);
+            $insertQuery .= "),\n";
+            $insertLog .= "),\n";
+            push @flatVals, @row;
+        }
+        $insertQuery = substr($insertQuery,0,-2);
+        $insertLog = substr($insertLog,0,-2);
+        $log->addLine($insertLog);
+        $dbHandler->updateWithParameters($insertQuery,\@flatVals);
+        undef @flatVals;
+    }
+}
+
+sub getForignKey
+{
+    my $table = shift;
+    my $colPos = shift;
+    my $value = shift;
+    my %convert_map = (
+    "import_status_0" => ("table" => "file_track", "colname" => "name"),
+    "file_track_1" => ("table" => "client", "colname" => "name"),
+    "file_track_2" => ("table" => "source", "colname" => "name"),
+    "source_2" => ("table" => "client", "colname" => "name"),
+    );
+    my $key = $table."_".$colPos;
+    if($convert_map{$key})
+    {
+        print "value \"$value\"";
+        $value = escapeData($value); #excape ticks
+        print " converted to: \"$value\"\n";
+        exit;
+        my $query = "SELECT id FROM $stagingTablePrefix"."_".$convert_map{$key}{"table"}." WHERE ".$convert_map{$key}{"colname"}." = '$value'";
+        my @results = @{$dbHandler->query($query)}
+        if(!$#results[0])
+        {
+            print "Error in seed data mapping\n";
+            exit;
+        }
+        my @row = @{$results[0]};
+        return @row[0];
+    }
+    return $value;
+}
+
+sub figureColumnsFromTable
+{
+    my $table = shift;
+    my @ret = ();
+    my $query = "
+        SELECT COLUMN_NAME 
+        FROM 
+        INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA='$databaseName'
+        AND TABLE_NAME='$stagingTablePrefix".'_'."$table'";
+    $log->addLine($query) if $debug;
+    my @results = @{$dbHandler->query($query)};
+    foreach(@results)
+    {
+        my @row = @{$_};
+        push @ret, @row[0];
+    }
+    return \@ret;
+}
+
+sub checkConfig
+{
+    my @reqs = ("logfile","db","dbhost","dbuser","dbpass","port","tmpspace");
+    my $valid = 1;
+    for my $i (0..$#reqs)
+    {
+        if(!$conf{@reqs[$i]})
+        {
+            print "Required configuration missing from conf file: ".@reqs[$i]."\n";
+            exit;
+        }
+    }
+}
+
+sub trim
+{
+    my $st = shift;
+    $st =~ s/^[\s\t]*(.*)/$1/;
+    $st =~ s/(.*)[\s\t]*$/$1/;
+    return $st;
+}
+
+sub dirtrav
+{
+	my @files = @{@_[0]};
+	my $pwd = @_[1];
+	opendir(DIR,"$pwd") or die "Cannot open $pwd\n";
+	my @thisdir = readdir(DIR);
+	closedir(DIR);
+	foreach my $file (@thisdir) 
+	{
+		if(($file ne ".") and ($file ne ".."))
+		{
+			if (-d "$pwd/$file")
+			{
+				push(@files, "$pwd/$file");
+				@files = @{dirtrav(\@files,"$pwd/$file")};
+			}
+			elsif (-f "$pwd/$file")
+			{			
+				push(@files, "$pwd/$file");			
+			}
+		}
+	}
+	return \@files;
+}
+
+sub figurePIDFileStuff
+{
+    if (-e $pidfile)
+    {
+        #Check the processes and see if there is a copy running:
+        my $thisScriptName = $0;
+        my $numberOfNonMeProcesses = scalar grep /$thisScriptName/, (split /\n/, `ps -aef`);
+        print "$thisScriptName has $numberOfNonMeProcesses running\n" if $debug;
+        # The number of processes running in the grep statement will include this process,
+        # if there is another one the count will be greater than 1
+        if($numberOfNonMeProcesses > 1)
+        {
+            print "Sorry, it looks like I am already running.\nIf you know that I am not, please delete $pidfile\n";
+            exit;
+        }
+        else
+        {
+            #I'm really not running
+            unlink $pidFile;
+        }
+    }
+}
+
+
+sub alertErrorEmail
+{
+    my $error = shift;
+    my @tolist = ($conf{"alwaysemail"});
+    my $email = new email($conf{"fromemail"},\@tolist,1,0,\%conf);
+    $email->send("Automatic Record Load Utility - $vendor Import Report Job # $jobid - ERROR","$error\r\n\r\n-Friendly MOBIUS Server-");
+}
+
+
+sub DESTROY
+{
+    print "I'm dying, deleting PID file $pidFile\n";
+    unlink $pidFile;
+}
+
+exit;
